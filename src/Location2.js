05e78aac27f5c3725bb4c232877d1043faf1e246
Dhanushk19 <95034453+Dhanushk19@users.noreply.github.com>
2023-10-20 23:18:24 +0530

    ss

--- src/Location2.js
+++ src/Location2.js
@@ -1,6 +1,8 @@
 import React from 'react';
 function Location2(){
     return(<>
-    <h1>Location2</h1></>);
+    <h1>Location2</h1>
+    <iframe src="https://demo.thingsboard.io/dashboard/6b1a9d40-6605-11ee-9d15-df6249c08ea6?publicId=47047900-455b-11ee-9079-75f587c23e37" title="description" position= "absolute" top="0" right="0"  bottom="0" left="0" width="100%" height="800"></iframe>
+    </>);
 }
 export default Location2;
--- src/Location3.js
+++ src/Location3.js
@@ -1,6 +1,7 @@
 import React from 'react';
 function Location3(){
     return(<>
-    <h1>Location3</h1></>);
+    <h1>Location3</h1>
+    <iframe src="https://demo.thingsboard.io/dashboard/2f6d5810-6dd9-11ee-9242-93ef3c64501e?publicId=47047900-455b-11ee-9079-75f587c23e37" title="description" position= "absolute" top="0" right="0"  bottom="0" left="0" width="100%" height="800"></iframe></>);
 }
 export default Location3;
